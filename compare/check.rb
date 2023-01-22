d29dd1f63ec19a0ad311ee357565a075c962fb30
Eliah Kagan <degeneracypressure@gmail.com>
2023-01-21 23:26:21 -0500

    Revert "Deliberate Ruby style fails to test CI"
    This reverts commit cd1be70215a2e9b0c814d4f96d94530083e94b1a.

--- compare/check.rb
+++ compare/check.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 # SPDX-License-Identifier: 0BSD
 
@@ -7,7 +8,7 @@ $VERBOSE = true
 require 'json'
 
 def run_check
-  python_path = File.expand_path('../python/python-embedding.json',  __dir__)
+  python_path = File.expand_path('../python/python-embedding.json', __dir__)
   java_path = File.expand_path('../java/java-embedding.json', __dir__)
   equal = JSON(File.read(python_path)) == JSON(File.read(java_path))
   printf "The embeddings %s equal.\n", (equal ? 'ARE' : 'are NOT')
