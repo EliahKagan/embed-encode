cd1be70215a2e9b0c814d4f96d94530083e94b1a
Eliah Kagan <degeneracypressure@gmail.com>
2023-01-21 23:21:26 -0500

    Deliberate Ruby style fails to test CI

--- compare/check.rb
+++ compare/check.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# frozen_string_literal: true
 
 # SPDX-License-Identifier: 0BSD
 
@@ -8,7 +7,7 @@ $VERBOSE = true
 require 'json'
 
 def run_check
-  python_path = File.expand_path('../python/python-embedding.json', __dir__)
+  python_path = File.expand_path('../python/python-embedding.json',  __dir__)
   java_path = File.expand_path('../java/java-embedding.json', __dir__)
   equal = JSON(File.read(python_path)) == JSON(File.read(java_path))
   printf "The embeddings %s equal.\n", (equal ? 'ARE' : 'are NOT')
